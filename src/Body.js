59a8d5296e1d9aa3f02d89f768e80b836719f488
ssokkara159 <cdn.shiva13@gmail.com>
2024-03-12 14:37:51 +0530

    added css stylinf to Body component

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import "./body.css";
 
 const Body = () => {
-  return <div>Body</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const handleMinusStep = () => {
+    setStep(step - 1);
+  };
+  const handlePlusStep = () => {
+    setStep(step + 1);
+  };
+
+  const handleMinusCount = () => {
+    setCount(count - step);
+  };
+  const handlePlusCount = () => {
+    setCount(count + step);
+  };
+
+  const date = new Date("March 12 2024");
+
+  date.setDate(date.getDate() + count);
+  const reqDate = date.toDateString();
+
+  return (
+    <div>
+      <div className="container">
+        <div className="steps">
+          <button onClick={handleMinusStep}>-</button>
+          <span>{step}</span>
+          <button onClick={handlePlusStep}>+</button>
+        </div>
+        <div className="count">
+          <button onClick={handleMinusCount}>-</button>
+          <span>{count}</span>
+          <button onClick={handlePlusCount}>+</button>
+        </div>
+        <span className="date">
+          {count === 0
+            ? `Today is `
+            : count >= 1
+            ? `The date after ${count} days`
+            : `The date Ago ${count} days`}
+        </span>
+        <span>{reqDate}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Body;
